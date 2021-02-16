1d80225d383d46cc01b964468c79bac587177f84
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-02-16 16:01:31 +0530

    Add files via upload

---
+++ CODING AND BEST PRACTICE.sql
@@ -0,0 +1,11 @@
+use sales;
+CREATE TABLE IF NOT EXISTS test (
+    numbers	 INT(10),
+    words 	 VARCHAR(10)
+);
+
+SELECT 
+	*
+FROM 
+   test;
+DROP TABLE test;
---
+++ DEFAULT Constraint Exercise.sql
@@ -0,0 +1,27 @@
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email_address VARCHAR(255),
+    number_of_complaints INT,
+PRIMARY KEY (customer_id)
+);
+
+ALTER TABLE customers
+ADD COLUMN gender ENUM('M', 'F') AFTER last_name;
+
+ 
+
+INSERT INTO customers (first_name, last_name, gender, email_address, number_of_complaints)
+VALUES ('John', 'Mackinley', 'M', 'john.mckinley@365careers.com', 0)
+
+CREATE TABLE companies
+(
+    company_id VARCHAR(255),
+    company_name VARCHAR(255) DEFAULT 'X',
+    headquarters_phone_number VARCHAR(255),
+PRIMARY KEY (company_id),
+UNIQUE KEY (headquarters_phone_number)
+);
+
+DROP TABLE companies;
