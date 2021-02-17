d121d4e0b180e43e9fc68a2b1fd14d7b14b59a45
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-02-17 17:18:03 +0530

    Add files via upload

---
+++ Inserting Data into a New Table.sql
@@ -0,0 +1,27 @@
+SELECT 
+    *
+FROM
+    departments
+LIMIT 10;
+
+CREATE TABLE departments_dup
+(    
+dept_no CHAR(4) NOT NULL,
+dept_name VARCHAR(40) NOT NULL
+);
+
+SELECT 
+    *
+FROM
+    departments_dup;
+    
+INSERT INTO departments_dup
+( 
+   dept_no,
+   dept_name
+)
+SELECT 
+* 
+FROM 
+    departments;
+   
