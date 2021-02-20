5c7c478e56915f80616ae380dd25afc92166fc0e
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-02-20 15:53:48 +0530

    Add files via upload

---
+++ Delete Statement Part-1.sql
@@ -0,0 +1,20 @@
+USE employees;
+
+COMMIT;
+SELECT 
+    *
+from
+    employees
+WHERE
+    emp_no = 999903;
+
+DELETE FROM employees 
+WHERE
+    emp_no = 999903;
+
+SELECT 
+    *
+FROM
+    titles
+WHERE
+    emp_no = 999903;
