a5e6e479e4f465fea896084d029ea240d32516c0
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-02-21 15:00:16 +0530

    Add files via upload

---
+++ Max() and Min() Exercise.sql
@@ -0,0 +1,14 @@
+# 1. Which is the lowest employee number in the database?
+
+
+SELECT
+    MIN(emp_no)
+FROM
+    employees;
+
+
+# 2. Which is the highest employee number in the database?
+SELECT 
+    MAX(emp_no)
+FROM
+    employees;
---
+++ Max() and Min().sql
@@ -0,0 +1,10 @@
+USE employees;
+SELECT 
+    MAX(salary)
+FROM
+    salaries;
+    
+SELECT 
+    MIN(salary)
+FROM
+    salaries;
