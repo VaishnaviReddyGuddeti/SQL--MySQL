ccb40b44de1fdeaeefa945bb143f5cbcaafa4a18
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-02-23 15:30:44 +0530

    Add files via upload

---
+++ INNER JOIN() Exercise.sql
@@ -0,0 +1,11 @@
+# Extract a list containing information about all managers’ employee number, first and last name, department number, and hire date. 
+SELECT
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    e.hire_date
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no;
---
+++ INNER JOIN().sql
@@ -0,0 +1,15 @@
+# dept_manager_dup
+SELECT 
+    *
+FROM
+    dept_manager_dup
+ORDER BY dept_no;
+
+
+# departments_dup
+SELECT 
+    *
+FROM
+    departments_dup
+ORDER BY dept_no;
+
