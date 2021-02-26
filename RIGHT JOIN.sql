a3ca97996e5f157d2af43a64c14b067b26d3fd7e
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-02-26 16:39:29 +0530

    Add files via upload

---
+++ RIGHT JOIN.sql
@@ -0,0 +1,16 @@
+# RIGHT JOIN
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+       RIGHT JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+ORDER BY dept_no;
+
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+       RIGHT OUTER JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+ORDER BY dept_no;
