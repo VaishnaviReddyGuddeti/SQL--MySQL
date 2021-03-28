80d4109a8821122cac32676efcd74e7de1ced4b5
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-03-28 20:12:18 +0530

    Add files via upload

---
+++ Stored Routines.sql
@@ -0,0 +1,2 @@
+# Stored Routines : an SQL statement that can be stored on the database server 
+# Stored Routines are of 2 types they are 1.stored procedures: which is equal to procedures and 2.functions: Which are user defined functions.
