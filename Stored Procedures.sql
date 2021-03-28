a3f83a12d3b9759259464b373ee12721cee33963
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-03-28 20:38:39 +0530

    Add files via upload

---
+++ Stored Procedures.sql
@@ -0,0 +1,14 @@
+USE employees;
+DROP PROCEDURE IF EXISTS select_employees;
+
+DELIMITER $$
+CREATE PROCEDURE select_employees()
+BEGIN
+             SELECT * FROM employees
+             LIMIT 1000;
+END$$
+DELIMITER ;
+
+call employees.select_employees();
+
+call select_employees();
