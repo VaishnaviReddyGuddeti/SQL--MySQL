058370fee8eb87bc41a7516143407d40f8626ae7
VaishnaviReddyGuddeti <67649206+VaishnaviReddyGuddeti@users.noreply.github.com>
2021-04-06 10:50:43 +0530

    Add files via upload

---
+++ The CASE Statement.sql
@@ -0,0 +1,63 @@
+use employees;
+SELECT 
+    emp_no,
+    first_name,
+    last_name,
+    CASE
+        WHEN gender = 'M' THEN 'Male'
+        ELSE 'Female'
+    END AS gender
+FROM
+    employees;
+    
+SELECT 
+    emp_no,
+    first_name,
+    last_name,
+    CASE gender
+        WHEN gender = 'M' THEN 'Male'
+        ELSE 'Female'
+    END AS gender
+FROM
+    employees;
+    
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    CASE
+        WHEN dm.emp_no IS NOT NULL THEN 'Manager'
+        ELSE 'Employee'
+    END AS is_manager
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager dm ON dm.emp_no = e.emp_no
+WHERE
+    e.emp_no > 109990;
+    
+SELECT 
+    emp_no,
+    first_name,
+    last_name,
+    IF(gender = 'M', 'Male', 'Female') AS gender
+FROM
+    employees;
+    
+SELECT 
+    dm.emp_no,
+    e.first_name,
+    e.last_name,
+    MAX(s.salary) - MIN(s.salary) AS salary_difference,
+    CASE
+        WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'Salary was raised by morre that Rs30,000'
+        WHEN MAX(s.salary) - MIN(s.salary) BETWEEN 20000 AND 30000 THEN 'Salary was raised by more than Rs20,000 but less than Rs30,000'
+        ELSE 'Salary was raised by less than Rs20,000'
+    END AS salary_increase
+FROM
+    dept_manager dm
+        JOIN
+    employees e ON e.emp_no = dm.emp_no
+        JOIN
+    salaries s ON s.emp_no = dm.emp_no
+GROUP BY s.emp_no;
